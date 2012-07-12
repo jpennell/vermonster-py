7ea634ce76454764b8326c0398c54bc10469b636
James Pennell <pennell.james@gmail.com>
2012-07-11 23:27:39 -0400

    Update model comments

--- vermonster/models.py
+++ vermonster/models.py
@@ -3,6 +3,10 @@ import json
 
 
 class Client(object):
+    """
+    Vermonster Client
+    - A consumer of the Cheddar API (http://cheddarapp.com)
+    """
     def __init__(self, oauth_id, oauth_secret, token=None):
         self.oauth_id = oauth_id
         self.oauth_secret = oauth_secret
@@ -13,13 +17,22 @@ class Client(object):
         self.tasks = AllTasksManager(client=self)
 
     def ping(self):
+        """
+        Ping Cheddar api and return a status code
+        """
         r = requests.get('https://api.cheddarapp.com/')
         return r.status_code
 
     def get_authorization_url(self):
+        """
+        Get Cheddar api authorization url
+        """
         return '%s/oauth/authorize?client_id=%s' % (self.connection, self.oauth_id)
 
     def get_token(self, code):
+        """
+        Get authorization token for code from oauth endpoint
+        """
         #Set up request
         headers = {'grant_type': 'authorization_code', 'code': code}
         url = '%s/oauth/token' % self.connection
@@ -31,14 +44,28 @@ class Client(object):
         self.token = r.body['access_token']
 
     def is_authorized(self):
+        """
+        Check if user is authorized
+        """
         return True if self.token else False
 
 
 class ListManager(object):
+    """
+    List manager: Manages Cheddar lists from api
+    """
     def __init__(self, client):
         self.client = client
 
     def all(self, include_archived=False):
+        """
+        Get all Cheddar lists
+
+        Parameters:
+
+        include_archived=False
+        - Set to True if you wish to return all lists, even if they are archived
+        """
         #Set up request
         headers = {'Authorization': 'Bearer %s' % self.client.token}
         url = '%s/v1/lists' % self.client.connection
@@ -53,27 +80,55 @@ class ListManager(object):
             list.client = self
             lists.append(list)
 
+        #Filter lists
         if not include_archived:
             lists = filter(lambda l: not l.archived_at, lists)
 
         return lists
 
     def get(self, id):
+        """
+        Get a single Cheddar list
+
+        Parameters:
+
+        id:
+        - Id of the cheddar list, ie '42'
+        """
         pass
 
     def create(self, title):
+        """
+        Create a cheddar list
+
+        Parameters:
+
+        title:
+        - Title of the list to create
+        """
         pass
 
     def reorder(self, list_ids):
+        """
+        Reorder Cheddar lists
+
+        Parameters:
+
+        list_ids:
+        - Ordered list of list ids
+        """
         pass
 
 
 class List(object):
+    """
+    List model
+    """
     def __init__(self, client=None):
         self.title = None
         self.archived_at = None
         self.client = client
-        self.tasks = TaskManager(client=self.client)
+        self.tasks = ListTaskManager(client=self.client)
 
     def __unicode__(self):
         return self.title
@@ -85,16 +140,38 @@ class List(object):
         return '<List: %s>' % self.title
 
     def update(self, title):
+        """
+        Update list
+
+        Parameters:
+
+        title:
+        - New title for the list
+        """
         pass
 
     def archive(self):
+        """
+        Archive list
+        """
         pass
 
-    def create_task(self):
+    def create_task(self, text):
+        """
+        Create task
+
+        Parameters:
+
+        text:
+        - Text for the task
+        """
         pass
 
     @staticmethod
     def decode_from_json(json_string):
+        """
+        Decode list from JSON
+        """
         list = List()
         list.title = json_string['title']
         list.archived_at = json_string['archived_at']
@@ -102,6 +179,9 @@ class List(object):
 
 
 class Task(object):
+    """
+    Task model
+    """
     def __init__(self):
         self.text = None
 
@@ -115,41 +195,96 @@ class Task(object):
         return '<Task: %s>' % self.text
 
     def update(self, text):
+        """
+        Update task
+
+        Parameters:
+
+        text:
+        - Text for the task
+        """
         pass
 
     def reorder(self, task_ids):
-        pass
+        """
+        Reorder tasks
 
-    def archive_completed(self):
-        pass
+        Parameters:
 
-    def archive_all(self):
+        task_ids:
+        - Ordered list of task ids
+        """
         pass
 
     @staticmethod
     def decode_from_json(json_string):
+        """
+        Decode task model from JSON
+        """
         task = Task()
         task.title = json_string['text']
         return task
 
 
-class TaskManager(object):
+class ListTaskManager(object):
     def __init__(self, client):
         self.client = client
 
     def all(self):
+        """
+        Get all tasks
+        """
         pass
 
     def get(self, id):
+        """
+        Get single task
+
+        Parameters:
+
+        id:
+        - Id of task
+        """
         pass
 
     def create(self, text):
+        """
+        Create task
+
+        Parameters:
+
+        text:
+        - Text for task
+        """
+        pass
+
+    def archive_completed(self):
+        """
+        Archive completed tasks
+        """
+        pass
+
+    def archive_all(self):
+        """
+        Archive all tasks
+        """
         pass
 
 
 class AllTasksManager(object):
+    """
+    All tasks manager
+    """
     def __init__(self, client):
         self.client = client
 
     def get(self, id):
+        """
+        Get task
+
+        Parameters:
+
+        id:
+        - Id of task
+        """
         pass
