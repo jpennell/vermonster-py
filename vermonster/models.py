6614e339b6e621627418ad1987f5f6ac92d660a1
James Pennell <pennell.james@gmail.com>
2012-07-25 20:28:29 -0400

    Work on #1 - Add get authentication url

--- vermonster/models.py
+++ vermonster/models.py
@@ -5,6 +5,7 @@ import json
 class Client(object):
     """
     Vermonster client
+    - A consumer of the Cheddar API (http://cheddarapp.com)
     """
     def __init__(self, oauth_id, oauth_secret, token=None):
         """
@@ -16,17 +17,47 @@ class Client(object):
         oauth_secret: oauth client secret, received when registering an app at http://cheddarapp.com
         token: access token (if already received) default None
         """
+
+        if not oauth_id or not isinstance(oauth_id, str):
+            raise ValueError('Expected valid oauth_id')
+
+        if not oauth_secret or not isinstance(oauth_secret, str):
+            raise ValueError('Expected valid oauth_secret')
+
+        if token and not isinstance(token, str):
+            raise ValueError('Expected valid token')
+
         self.oauth_id = oauth_id
         self.oauth_secret = oauth_secret
         self.token = token
 
-    def get_authentication_url():
+    def get_authentication_url(self, redirect_uri=None, state=None):
         """
         Get cheddar authorization url
-        """
-        pass
 
-    def get_access_token(code):
+        Parameters
+
+        redirect_uri: An optional redirect URI. If none is provided, the one used when you registered your app will be used. If one is provided, it must contain the app's redirect URI as its prefix.
+        state: Put whatever you want here. It will be passed back when the users authorized or declines your request. A common practice is to use some sort of identifier to identify the user making the request. This value will be truncated to 255 characters.
+        """
+
+        if redirect_uri and not isinstance(redirect_uri, str):
+            raise ValueError('Expected valid redirect_uri')
+
+        if state and not isinstance(state, str):
+            raise ValueError('Expected valid state')
+
+        url = "https://api.cheddarapp.com/oauth/authorize?client_id=%s" % self.oauth_id
+
+        if redirect_uri:
+            url = url + "&redirect_uri=%s" % redirect_uri
+
+        if state:
+            url = url + "&state=%s" % state
+
+        return url
+
+    def get_access_token(self, code):
         """
         Get access token
 
@@ -36,7 +67,7 @@ class Client(object):
         """
         pass
 
-    def is_authenticated():
+    def is_authenticated(self):
         """
         Is user authenticated?
         """
